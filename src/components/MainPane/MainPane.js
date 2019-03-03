ef39fb3441e96eabd655a1d324fe10fa404cb0f9
AWolf81 <awolf2904@gmail.com>
2019-03-03 02:32:07 +0100

    added share & comments

--- src/components/MainPane/MainPane.js
+++ src/components/MainPane/MainPane.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { COLORS } from '../../constants';
 
 export default ({ children }) => (
-  <div style={{ maxWidth: 1180, margin: '0 auto' }}>{children}</div>
+  <div
+    style={{ maxWidth: 1180, margin: '0 auto', background: COLORS.snow[100] }}
+  >
+    {children}
+  </div>
 );
---
+++ src/constants.js
@@ -0,0 +1,25 @@
+export const COLORS = {
+  /* Coolors Exported Palette - coolors.co/fcfafa-c8d3d5-a4b8c4-6e8387-0cca4a */
+  snow: {
+    100: 'rgba(252, 250, 250, 1)',
+  },
+  lightGray: {
+    100: 'rgba(200, 211, 213, 1)',
+  },
+  wildBlue: {
+    100: 'rgba(164, 184, 196, 1)',
+  },
+  darkGray: {
+    100: 'rgba(110, 131, 135, 1)',
+  },
+  mintGreen: {
+    100: 'rgba(12, 202, 74, 1)',
+  },
+  black: {
+    100: '#171717',
+    80: '#454545',
+    60: '#737272',
+    40: '#A1A0A0',
+    20: '#CFCDCD',
+  },
+};
